2afca3765c6e806a7f29cd4475e4168515985603
Noval Bintang <m.novalbs@gmail.com>
2021-08-02 16:50:52 +0700

    add mock

---
+++ html/mock.js
@@ -0,0 +1,78 @@
+window.templateConfig = {
+  cover: {
+    desktop: 'https://ik.imagekit.io/mnovalbs/gallery3_miEYkuEMa.png',
+    mobile: 'https://ik.imagekit.io/mnovalbs/gallery1_UfnFFXqXl.png',
+  },
+  bride: {
+    social: {
+      instagram: 'https://instagram.com',
+      twitter: 'https://twitter.com',
+      facebook: 'https://facebook.com',
+    },
+    photo: 'https://ik.imagekit.io/mnovalbs/bride_QNQxwUCY7o.png',
+    name: 'Zidni Tazkiyah',
+    shortName: 'Zidni',
+    parent: 'Bpk John Doe dan Ibu Jane Doe',
+    description:
+      'Nulla porta diam sed lacinia hendrerit. Sed congue, urna in placerat aliquet, enim dolor rhoncus dolor, eu fermentum nisl ipsum in libero. Nulla facilisi. Nam feugiat nunc ac condimentum accumsan.',
+  },
+  groom: {
+    social: {
+      instagram: 'https://instagram.com',
+      twitter: 'https://twitter.com',
+      facebook: 'https://facebook.com',
+    },
+    photo: 'https://ik.imagekit.io/mnovalbs/groom_1d6C2z4Anq.png',
+    name: 'Hafidh Aulia W',
+    shortName: 'Hafidh',
+    parent: 'Bpk Aris dan Ibu Jane Doe',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis, dui et hendrerit scelerisque, nulla sem bibendum mauris, in finibus leo risus id lorem. Proin sed finibus lorem.',
+  },
+  event: [
+    {
+      time: { start: '10:00', end: '11:30' },
+      location: {
+        name: 'Hotel Ciputra Semarang',
+        address:
+          'Hotel Ciputra Semarang, Jalan Simpang Lima, Pekunden, Semarang City, Central Java, Indonesia',
+        coordinate: '-6.98869528617265,110.42392239391786',
+      },
+      name: 'Akad Nikah',
+      date: '2021-10-24',
+    },
+  ],
+  gallery: [
+    'https://ik.imagekit.io/mnovalbs/gallery1_G6ezWQxW5.png',
+    'https://ik.imagekit.io/mnovalbs/gallery2__Hmqbg9oG.png',
+    'https://ik.imagekit.io/mnovalbs/gallery3_YXopNUQCY.png',
+    'https://ik.imagekit.io/mnovalbs/gallery4_leTuMrdXo.png',
+    'https://ik.imagekit.io/mnovalbs/gallery5_WCLqZBqhG.png',
+    'https://ik.imagekit.io/mnovalbs/gallery6_qNDll6S98T.png',
+    'https://ik.imagekit.io/mnovalbs/gallery7_gW7NqVpa69.png',
+  ],
+  amplop: [
+    { accountNumber: '123456', accountName: 'John BCA' },
+    {
+      bankAccount: 'jenius',
+      accountNumber: '$johndoe',
+      accountName: 'Doe Jenius',
+    },
+    {
+      bankAccount: 'mandiri',
+      accountNumber: '432234',
+      accountName: 'Jane Mandiri',
+    },
+  ],
+  story: [
+    {
+      image: 'https://ik.imagekit.io/mnovalbs/story_rE9kSOxWw.png',
+      time: '2014',
+      title: 'Pandangan Pertama',
+      description:
+        'Maecenas non maximus quam. Mauris a ornare nisi. Aliquam at enim augue. Nullam porttitor ut velit sit amet hendrerit. Praesent velit leo, hendrerit a magna sit amet, porta ultricies lacus. Pellentesque enim justo, congue quis pretium quis, venenatis eu urna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque id leo vehicula, vehicula velit sed, scelerisque est.',
+    },
+  ],
+  video: ['https://www.youtube.com/watch?v=9tCFRiUFTwk'],
+  date: '2021-10-24',
+};
