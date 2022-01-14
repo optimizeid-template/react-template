25b3431f6b954f1308d80f188ec3d25a22f16f95
Noval Bintang <m.novalbs@gmail.com>
2022-01-14 15:41:25 +0700

    logo list

---
+++ src/components/LogoList/LogoList.js
@@ -0,0 +1,75 @@
+import styled from '@emotion/styled';
+import React from 'react';
+import Container from '../Container/Container';
+
+const Wrapper = styled.div`
+  background: #f9f9f9;
+  border-radius: 24px;
+  padding: 64px;
+  margin-top: 24px;
+  margin-bottom: 24px;
+  text-align: center;
+`;
+
+const Title = styled.h4`
+  font-weight: bold;
+  font-size: 32px;
+  line-height: 44px;
+  color: #373f41;
+  margin: 0;
+  margin-bottom: 16px;
+`;
+
+const Caption = styled.div`
+  font-size: 20px;
+  line-height: 27px;
+  color: #373f41;
+  margin-bottom: 40px;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  gap: 40px;
+  grid-template-columns: auto auto auto auto;
+  max-width: 700px;
+  margin: 0 auto;
+`;
+
+const LogoPlaceholder = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: 500;
+  width: 100%;
+  background: #fff;
+  height: 100px;
+  box-shadow: 0 3px 10px #eee;
+  border-radius: 8px;
+`;
+
+function LogoList() {
+  return (
+    <Container xl>
+      <Wrapper>
+        <Title>This is the long title for explaining what happen here</Title>
+        <Caption>
+          Explanation sentences for describing the condition around here. Maybe
+          it can be 2 sentences, no more.{' '}
+        </Caption>
+
+        <Grid>
+          <LogoPlaceholder>Logo</LogoPlaceholder>
+          <LogoPlaceholder>Logo</LogoPlaceholder>
+          <LogoPlaceholder>Logo</LogoPlaceholder>
+          <LogoPlaceholder>Logo</LogoPlaceholder>
+          <LogoPlaceholder>Logo</LogoPlaceholder>
+          <LogoPlaceholder>Logo</LogoPlaceholder>
+          <LogoPlaceholder>Logo</LogoPlaceholder>
+          <LogoPlaceholder>Logo</LogoPlaceholder>
+        </Grid>
+      </Wrapper>
+    </Container>
+  );
+}
+
+export default LogoList;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import HomeCover from '../components/HomeCover/HomeCover';
+import LogoList from '../components/LogoList';
 
 function Home() {
   return (
     <>
       <HomeCover />
+
+      <LogoList />
     </>
   );
 }
